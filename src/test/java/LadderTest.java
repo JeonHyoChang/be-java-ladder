f501a16b29b112d842859c8ff4b3def80011b8a7
JeonHyoChang <jhc1426@naver.com>
2023-03-08 16:26:41 +0900

    ✨feat: 테스트 틀 생성

---
+++ src/test/java/LadderTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+    @Test
+    @DisplayName("뭔가 시작함")
+    void dddd(){
+        System.out.println("시작");
+    }
+
+    @Test
+    @DisplayName("시작후 또 시작함")
+    void qqq(){
+        System.out.println("또 시작");
+    }
+}
